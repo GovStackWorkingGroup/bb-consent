38cbb1c3b5b9fe13849981770f403c75c841eb3e
Benjamin Bach <benjamin@overtag.dk>
2023-11-30 20:32:48 +0100

    Add a comment and update some Python 2.7 example code artifacts

--- test/gherkin/features/steps/smoke.py
+++ test/gherkin/features/steps/smoke.py
@@ -4,7 +4,7 @@ import os
 from behave import given, when, then
 
 
-@given(u'A URL of a Consent Building Block instance')
+@given("A URL of a Consent Building Block instance")
 def step_impl(context):
 
     # We just use the API url given from the context, a more advanced
@@ -13,14 +13,19 @@ def step_impl(context):
     context.api_url = context.session.api_url
 
 
-@when(u'I call a basic public API endpoint')
+@when("I call a basic public API endpoint")
 def step_impl(context):
+    """
+    This method currently calls a well-known API with fixture data (policy ID=1).
+    However, we would like to call a GovStack status API instead, but this isn't yet a feature that has
+    been developed across building blocks.
+    """
     context.response = context.session.get(
         os.path.join(context.api_url, "service/policy/1/")
     )
 
 
-@then(u'I get some valid JSON data back')
+@then("I get some valid JSON data back")
 def step_impl(context):
     if not context.response.status_code == 200:
         raise AssertionError(f"Got status code {context.response.status_code}\n\nContent:\n{context.response.content}")

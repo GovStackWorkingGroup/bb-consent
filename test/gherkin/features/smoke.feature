280a32f12201bb36242665e7c4f8e565ec9a5a95
lruzicki <56487722+lruzicki@users.noreply.github.com>
2023-12-29 15:21:16 +0100

    TECH-1016: test review and data_agreement tests (#70)
    * TECH-1016: test review and data_agreement tests
    
    * TECH-1016: Test update of orb version
    
    * TECH-1016: Update release version of the orb
    
    * TECH-1016: negative test for data agreement added
    
    ---------
    
    Co-authored-by: Damian Borowiecki <dborowiecki@soldevelo.com>

--- test/gherkin/features/smoke.feature
+++ test/gherkin/features/smoke.feature
@@ -1,8 +1,20 @@
 @method=GET @endpoint=/service/policy/{policyId}/
-Feature: The Consent Building Block exists
+# Here we paste the title.
+Feature: The Consent Building Block Exists
+# Recommended - Here we can paste the user stories to make it more readable.
 
+
+# Background: This will be executed once for each
+# Given a URL of Consent BB.
+
+
+# We can extend it with the test category that may be useful with hooks like @consent_bb_exists
   @smoke
-  Scenario: The platform and its API exists
-      Given A URL of a Consent Building Block instance
+  Scenario: The Platform and Its API Exists
+  # Step can be parameterized; let's pass URL here.
+      Given A URL of a Consent Building Block instance 
       When I call a basic public API endpoint
       Then I get some valid JSON data back
+
+
+# Also, we can paste the example template here.
---
+++ test/gherkin/features/steps/example_steps.py
@@ -0,0 +1,31 @@
+# from behave import given, when, then
+
+# @given('some initial context')
+# def step_impl(context):
+#     # Setup code or context initialization
+#     pass
+
+# @given('some {condition}')
+# def step_impl(context, condition):
+#     # Setup code using the parameter {condition}
+#     context.condition = condition
+
+# @when('an action is taken')
+# def step_impl(context):
+#     # Code to perform an action
+#     pass
+
+# @when('an action is {action}')
+# def step_impl(context, action):
+#     # Code to perform an action based on {action}
+#     context.action = action
+
+# @then('expect a specific outcome')
+# def step_impl(context):
+#     # Assertion or verification code
+#     pass
+
+# @then('I expect {outcome}')
+# def step_impl(context, outcome):
+#     # Assertion or verification code with parameter {outcome}
+#     context.test_assertion(outcome)
